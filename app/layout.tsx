2c21f74c5e1af84b6cd7d1f9d385dac3db0224f3
Otibaco <orjifranklin42@gmail.com>
2025-06-20 05:41:58 +0100

    update to fix the failed deployment 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import { Toaster } from "sonner"
 
 import { Orbitron, Montserrat } from "next/font/google"
+import { Suspense } from "react"
 
 const orbitron = Orbitron({
   subsets: ["latin"],
@@ -27,7 +28,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={`{montserrat.variable}`}>
       <body cz-shortcut-listen="true">
-        <ReferralHandler />
+        <Suspense>
+          <ReferralHandler />
+        </Suspense>
+
         {children}
         <Toaster />
       </body>
